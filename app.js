59002303401933c20eb24abe6859289062c2e7c7
rashaalkyeam <rasha.alqyyam@gmail.com>
2021-04-28 14:48:17 +0300

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict'
+let nationality= prompt('Do you think that I have the Jordanian nationality?\n pleas enter yes or no, y or n').toLowerCase();
+let age= prompt('Do you think that I am less than 25 years old?\n pleas enter yes or no, y or n').toLowerCase();
+let live= prompt('Do you think I live in Amman?\n pleas enter yes or no, y or n').toLowerCase();
+let study= prompt('Do you think I studied computer science?\n pleas enter yes or no, y or n').toLowerCase();
+let car= prompt('Do you think I own a car?\n pleas enter yes or no, y or n').toLowerCase();
+
+
+
+if(nationality=='yes'||nationality=='y'){
+    console.log('What you think is correct')
+}else{
+    console.log('What you think is not true')
+    
+}
+if(age=='yes'||age=='y'){
+    console.log('What you think is correct')
+}else{
+    console.log('What you think is not true')
+    
+}
+if(live=='yes'||live=='y'){
+    console.log('What you think is correct')
+}else{
+    console.log('What you think is not true')
+    
+}
+if(study=='yes'||study=='y'){
+    console.log('What you think is correct')
+}else{
+    console.log('What you think is not true')
+    
+}
+if(car=='yes'||car=='y'){
+    console.log('What you think is correct')
+}else{
+    console.log('What you think is not true')
+    
+}
+
